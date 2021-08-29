99c544b2d0f08b305ac36dba38c06dea1c53cd55
RoaaMustafa <labushanab14@gmail.com>
2021-08-29 17:30:32 +0300

    did the blog and white board for the insertion sort

--- python/insertion-sort/tests/test_insertion_sort.py
+++ python/insertion-sort/tests/test_insertion_sort.py
@@ -1,5 +1,32 @@
 from insertion_sort import __version__
+from insertion_sort.insertion_sort import *
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_insertionsort():
+
+    arr=[7,8,9,0,4,2]
+    actual=insertionSort(arr)
+    expected=[0, 2, 4, 7, 8, 9]
+
+    assert expected==actual
+
+def test_edge_insertionsort():
+
+    arr=[]
+    actual=insertionSort(arr)
+    expected=[]
+
+    assert expected==actual
+
+def test_edge_odd_insertionsort():
+
+    arr=[13]
+    actual=insertionSort(arr)
+    expected=[13]
+
+    assert expected==actual
+
